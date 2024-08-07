dc9314665925953b5fe339c5973bcc770859ad30
Er. Vikas Prajapati <81795265+VikasPrajapati1998@users.noreply.github.com>
2024-08-07 13:24:16 +0530

    Add Main Index Page

--- Python/Django/my_tennis_club/members/urls.py
+++ Python/Django/my_tennis_club/members/urls.py
@@ -1,9 +1,8 @@
-from django.urls import path # type: ignore
+from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('members/', views.members, name="members"),
-    path('members/details/<int:id>', views.details, name='details')
+    path('', views.main, name='main'),
+    path('members/', views.members, name='members'),
+    path('members/details/<int:id>', views.details, name='details'),
 ]
-
-
--- Python/Django/my_tennis_club/members/views.py
+++ Python/Django/my_tennis_club/members/views.py
@@ -10,30 +10,28 @@ Views are usually put in a file called views.py located on your app's folder.
 # Take http requests and returns http response 
 
 
-from django.shortcuts import render # type: ignore
-from django.http import HttpResponse # type: ignore
-from django.template import loader # type: ignore
+from django.http import HttpResponse
+from django.template import loader
 from .models import Member
 
 def members(request):
-    '''
-    template = loader.get_template('myfirst.html')
-    # return HttpResponse("Hello World!")
-    return HttpResponse(template.render())
-    '''
-    mymembers = Member.objects.all().values()
-    template = loader.get_template("all_members.html")
-    context = {
-        'mymembers': mymembers,
-    }
-    return HttpResponse(template.render(context, request))
-
-
+  mymembers = Member.objects.all().values()
+  template = loader.get_template('all_members.html')
+  context = {
+    'mymembers': mymembers,
+  }
+  return HttpResponse(template.render(context, request))
+  
 def details(request, id):
-    mymember = Member.objects.get(id=id)
-    template = loader.get_template('details.html')
-    context = {
-        'mymember': mymember
-    }
-    return HttpResponse(template.render(context, request))
+  mymember = Member.objects.get(id=id)
+  template = loader.get_template('details.html')
+  context = {
+    'mymember': mymember,
+  }
+  return HttpResponse(template.render(context, request))
+  
+def main(request):
+  template = loader.get_template('main.html')
+  return HttpResponse(template.render())
+
 
