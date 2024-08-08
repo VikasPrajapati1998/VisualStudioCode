8579e8dbd133e227d7f3ac56aee20eb661688d87
Er. Vikas Prajapati <81795265+VikasPrajapati1998@users.noreply.github.com>
2024-08-08 12:45:10 +0530

    Django Admin

--- Python/Django/my_tennis_club/my_tennis_club/urls.py
+++ Python/Django/my_tennis_club/my_tennis_club/urls.py
@@ -14,6 +14,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
+
 from django.contrib import admin  # type: ignore
 from django.urls import include, path  # type: ignore
 
@@ -21,4 +23,5 @@ urlpatterns = [
     path('', include('members.urls')),
     path('admin/', admin.site.urls),
 ]
+
 
